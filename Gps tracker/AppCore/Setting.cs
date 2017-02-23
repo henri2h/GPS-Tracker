7b46f6224606b304748d55c19fd1b4ba1d52e391
Henri2h <henri.carnot@hotmail.fr>
2017-02-23 18:20:34 +0100

    new setting class

---
+++ Gps tracker/AppCore/Setting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gps_tracker.AppCore
+{
+    public class Setting
+    {
+        public bool autoSave { get; set; }
+        public bool enhancedMode { get; set; }
+    }
+}
