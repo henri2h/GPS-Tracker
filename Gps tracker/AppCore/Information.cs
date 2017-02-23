a1bcaaf1779ba536da61b0fb439f2747bd86b484
Henri2h <henri.carnot@hotmail.fr>
2017-02-23 19:42:29 +0100

    new information file

---
+++ Gps tracker/AppCore/Information.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gps_tracker.AppCore
+{
+    public class Information
+    {
+        public DateTime date { get; set; }
+
+        public double maxSpeed { get; set; }
+        public double currentSpeed { get; set; }
+        public double mediumSpeed { get; set; }
+
+
+        public double totalTravelDistance { get; set; }
+
+        public string output { get; set; }
+        public string Status { get; set; }
+
+        public bool isExtendedSessionOn { get; set; }
+        public point currentPoint { get; set; }
+    }
+}
