4c3c8f28bda25d0bc400ca2657ab6eb4add09a68
Henri2h <henri.carnot@hotmail.fr>
2017-02-26 20:05:06 +0100

    directoryObject class

---
+++ Gps tracker/UI/FilesView/directoryObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gps_tracker.UI.FilesView
+{
+    public class directoryObject
+    {
+        public string name { get; set; }
+        public string path { get; set; }
+        public bool isDirectory { get; set; }
+        public DateTime lastAccess { get; set; }
+    }
+}
